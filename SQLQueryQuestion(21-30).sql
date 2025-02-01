694e8f153e62b0472c87d6e76b0c2df67f45ba54
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2025-02-01 19:07:50 +0100

    Solved Qusetion22

--- SQLQueryQuestion(21-30).sql
+++ SQLQueryQuestion(21-30).sql
@@ -13,3 +13,20 @@ SELECT
 FROM dbo.Customers
 GROUP BY Country, City
 ORDER BY TotalCustomers DESC;
+
+/*
+22. What products do we have in our inventory that should be reordered?
+For now, just use the fields UnitsInStock and ReorderLevel, where
+UnitsInStock is less than the ReorderLevel, ignoring the fields
+UnitsOnOrder and Discontinued.
+Order the results by ProductID.
+*/
+
+SELECT
+	ProductID,
+	ProductName,
+	UnitsInStock,
+	ReorderLevel
+FROM dbo.Products
+WHERE UnitsInStock < ReorderLevel
+ORDER BY ProductID;
