ba3935a5bdb2162ff75518f302c85cf77fb9098d
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2025-02-01 19:24:22 +0100

    Solved Qusetion24

--- SQLQueryQuestion(21-30).sql
+++ SQLQueryQuestion(21-30).sql
@@ -50,3 +50,34 @@ SELECT
 FROM dbo.Products
 WHERE (UnitsInStock + UnitsOnOrder) <= ReorderLevel AND Discontinued = 0
 ORDER BY ProductID;
+
+/*
+24. A salesperson for Northwind is going on a business trip to visit
+customers, and would like to see a list of all customers, sorted by
+region, alphabetically.
+However, he wants the customers with no region (null in the Region
+field) to be at the end, instead of at the top, where you’d normally find
+the null values. Within the same region, companies should be sorted by
+CustomerID.
+*/
+
+SELECT
+	CustomerID,
+	CompanyName,
+	Region
+FROM dbo.Customers
+ORDER BY
+	CASE WHEN Region IS NULL THEN 1 ELSE 0 END,
+	REGION,
+	CompanyName;
+
+SELECT
+	CustomerID,
+	CompanyName,
+	Region,
+	CASE WHEN Region IS NULL THEN 1 ELSE 0 END AS ComputedField
+FROM dbo.Customers
+ORDER BY
+	ComputedField,
+	Region,
+	CompanyName;
