1053fbc2efa30c3d14e0e70afd01a97985ec64d9
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2025-02-01 17:51:49 +0100

    Solved Qusetion9

--- SQLQueryQuestion(1-10).sql
+++ SQLQueryQuestion(1-10).sql
@@ -74,6 +74,29 @@ orders where the ShipCountry is either France or Belgium.
 SELECT
 	OrderID,
 	CustomerID,
+	OrderDate,
 	ShipCountry
 FROM dbo.Orders
-WHERE ShipCountry IN('France','Belgium'); -- WHERE ShipCountry = 'France' OR WHERE ShipCountry = 'Belgium'
+WHERE ShipCountry = 'France' OR ShipCountry = 'Belgium';
+
+/*
+9. Now, instead of just wanting to return all the orders from France of
+Belgium, we want to show all the orders from any Latin American
+country. But we don’t have a list of Latin American countries in a table
+in the Northwind database. So, we’re going to just use this list of Latin
+American countries that happen to be in the Orders table:
+Brazil
+Mexico
+Argentina
+Venezuela
+It doesn’t make sense to use multiple Or statements anymore, it would
+get too convoluted. Use the In statement.
+*/
+
+SELECT
+	OrderID,
+	CustomerID,
+	OrderDate,
+	ShipCountry
+FROM dbo.Orders
+WHERE ShipCountry IN ('Brazil','Mexico','Argentina','Venezuela');
