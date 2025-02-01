e00dc186509c9293f4f3a19a0703dd1b80969768
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2025-02-01 18:06:32 +0100

    Solved Qusetion12

--- SQLQueryQuestion(11-20).sql
+++ SQLQueryQuestion(11-20).sql
@@ -11,5 +11,17 @@ SELECT
 	FirstName,
 	LastName,
 	Title,
-	FORMAT(BirthDate,'yyyy-MM-dd') AS DateOnlyBirthDate
+	CONVERT(DATE,BirthDate) AS DateOnlyBirthDate
+FROM dbo.Employees;
+
+/*
+12. Show the FirstName and LastName columns from the Employees table,
+and then create a new column called FullName, showing FirstName and
+LastName joined together in one column, with a space in-between.
+*/
+
+SELECT
+	FirstName,
+	LastName,
+	CONCAT_WS(' ',FirstName,LastName) AS FullName
 FROM dbo.Employees;
