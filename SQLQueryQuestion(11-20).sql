203fcb08cd507071043c36e561101922b5fab1d3
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2025-02-01 18:43:14 +0100

    Solved Qusetion19

--- SQLQueryQuestion(11-20).sql
+++ SQLQueryQuestion(11-20).sql
@@ -109,3 +109,21 @@ From dbo.Products AS P
 LEFT JOIN dbo.Suppliers AS S
 	ON P.SupplierID = S.SupplierID
 ORDER BY P.ProductID;
+
+/*
+19. We’d like to show a list of the Orders that were made, including the
+Shipper that was used. Show the OrderID, OrderDate (date only), and
+CompanyName of the Shipper, and sort by OrderID.
+In order to not show all the orders (there’s more than 800), show only
+those rows with an OrderID of less than 10300.
+*/
+
+SELECT 
+	O.OrderID,
+	CONVERT(DATE,O.OrderDate) AS OrderDate,
+	S.CompanyName
+FROM dbo.Orders AS O
+LEFT JOIN dbo.Shippers AS S
+	ON O.ShipVia = S.ShipperID
+WHERE OrderID < 10300
+ORDER BY OrderID;
