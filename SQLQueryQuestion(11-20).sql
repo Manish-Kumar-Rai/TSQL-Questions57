5f171d6c52d2d98ed4083341acd7e3880986c86a
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2025-02-01 18:30:24 +0100

    Solved Qusetion18

--- SQLQueryQuestion(11-20).sql
+++ SQLQueryQuestion(11-20).sql
@@ -91,3 +91,21 @@ SELECT
 	COUNT(1) AS TotalContactTitle
 FROM dbo.Customers
 GROUP BY ContactTitle;
+
+/*
+18. We’d like to show, for each product, the associated Supplier. Show the
+ProductID, ProductName, and the CompanyName of the Supplier. Sort
+by ProductID.
+This question will introduce what may be a new concept, the Join clause
+in SQL. The Join clause is used to join two or more relational database
+tables together in a logical way.
+*/
+
+SELECT
+	P.ProductID,
+	P.ProductName,
+	S.CompanyName
+From dbo.Products AS P
+LEFT JOIN dbo.Suppliers AS S
+	ON P.SupplierID = S.SupplierID
+ORDER BY P.ProductID;
