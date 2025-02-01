9eb185b69a592b75266e542259c0973e6e747d33
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2025-02-01 19:28:57 +0100

    Solved Qusetion25

--- SQLQueryQuestion(21-30).sql
+++ SQLQueryQuestion(21-30).sql
@@ -81,3 +81,19 @@ ORDER BY
 	ComputedField,
 	Region,
 	CompanyName;
+
+/*
+25. Some of the countries we ship to have very high freight charges. We'd
+like to investigate some more shipping options for our customers, to be
+able to offer them lower freight charges. Return the three ship countries
+with the highest average freight overall, in descending order by average
+freight.
+*/
+
+SELECT
+	TOP (3)
+	ShipCountry,
+	AVG(Freight) AS Avg_Freight
+FROM dbo.Orders
+GROUP BY ShipCountry
+ORDER BY AVG(Freight) DESC;
