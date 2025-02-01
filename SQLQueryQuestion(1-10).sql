ece5c785334cad7a0a599efde45638b0818ea449
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2025-02-01 17:29:47 +0100

    Solved Qusetion3

--- SQLQueryQuestion(1-10).sql
+++ SQLQueryQuestion(1-10).sql
@@ -2,8 +2,19 @@
 
 -- 1. Which shippers do we have?
 
-Select * From dbo.Shippers;
+SELECT * From dbo.Shippers;
 
 -- 2. Certain fields from Categories?
 
-Select CategoryName,[Description] FROM dbo.Categories;
+SELECT CategoryName,[Description] FROM dbo.Categories;
+
+/* 3. We’d like to see just the FirstName, LastName, and HireDate of all the
+employees with the Title of Sales Representative. Write a SQL
+statement that returns only those employees. */
+
+SELECT
+	FirstName,
+	LastName,
+	FORMAT(HireDate,'yyyy-MM-dd') AS HireDate
+FROM dbo.Employees
+WHERE Title = 'Sales Representative';
