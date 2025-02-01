e1df907ec98fea3a2c047916648754a4893cbbbe
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2025-02-01 19:11:20 +0100

    Solved Qusetion23

--- SQLQueryQuestion(21-30).sql
+++ SQLQueryQuestion(21-30).sql
@@ -30,3 +30,23 @@ SELECT
 FROM dbo.Products
 WHERE UnitsInStock < ReorderLevel
 ORDER BY ProductID;
+
+/*
+23. Now we need to incorporate these fields—UnitsInStock, UnitsOnOrder,
+ReorderLevel, Discontinued—into our calculation. We’ll define
+“products that need reordering” with the following:
+UnitsInStock plus UnitsOnOrder are less than or equal to
+ReorderLevel
+The Discontinued flag is false (0).
+*/
+
+SELECT
+	ProductID,
+	ProductName,
+	UnitsInStock,
+	UnitsOnOrder,
+	ReorderLevel,
+	Discontinued
+FROM dbo.Products
+WHERE (UnitsInStock + UnitsOnOrder) <= ReorderLevel AND Discontinued = 0
+ORDER BY ProductID;
